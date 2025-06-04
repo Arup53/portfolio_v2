4aef2d7aafacb20fe253f263b0f6a956746393eb
Arup53 <arupkumarhowlader12@gmail.com>
2025-06-04 23:19:27 +0600

    add script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Arup_dev",
@@ -16,6 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Other <meta> tags */}
+        <Script
+          defer
+          data-domain="portfolio-v2-omega-smoky.vercel.app"
+          src="https://analytics-sandy-five.vercel.app/tracking-script.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={` bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         <Navbar />
         {children}
